50bdceb522347b97fc09324748695e96029704ee
Naggelus <foreyoue@gmail.com>
2020-01-17 15:20:22 +0100

    exprimental select

--- recon1.bat
+++ recon1.bat
@@ -2,6 +2,7 @@
 
 call:init
 call:basic_info
+call:select
 pause
 goto:eof
 
@@ -11,4 +12,12 @@ exit /b
 
 :basic_info
 powershell /NoProfile /NoLogo /command "Write-Host -NoNewLine -ForegroundColor DarkYellow 'Username: '; Write-Host $env:USERNAME; Write-Host -NoNewLine -ForegroundColor DarkYellow 'Computername: '; Write-Host $env:COMPUTERNAME; Write-Host -NoNewLine -ForegroundColor DarkYellow 'Windows Edition: '; gwmi win32_operatingsystem | %% caption; Write-Host -NoNewLine -ForegroundColor DarkYellow 'Windows Version: '; (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion').ReleaseId;"
+echo.
+echo Options:
+echo 1) ipconfig
+exit /b
+
+:select
+choice /C 123456789 /N
+echo %ERRORLEVEL%
 exit /b
